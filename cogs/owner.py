60de39f25c5dbf74144ef0dd98ffecc4b10674d6
William C. <thebeast47yt@gmail.com>
2024-02-14 14:53:59 -0500

    updated reminder command and some other stuff like adding an eval command, i really dont remember, good afternoon :)

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,4 +1,4 @@
-import voltage, asyncio, random, time, psutil, pymongo, json, datetime
+import voltage, asyncio, random, time, psutil, pymongo, json, datetime, io, contextlib
 from voltage.ext import commands
 
 def setup(client) -> commands.Cog:
@@ -10,51 +10,84 @@ def setup(client) -> commands.Cog:
   @owner.command()
   async def statz(ctx):
     """Different from normal stats, the normal one shows the stats of the bot, this one shows complex stats. Like CPU usage and whatnot."""
-    with open("json/data.json", "r") as f:
-      uptime = json.load(f)['uptime']
-    embed = voltage.SendableEmbed(
-      title=f"{client.user.name}'s Stats",
-      description=f"""
-## Computer Based Stats
-> CPU Usage: `{psutil.cpu_percent()}%`
-> RAM Usage: `{psutil.virtual_memory().percent}%`
-> Disk Usage: `{psutil.disk_usage('/').percent}%`
-      
-## Bot Stats
-> Servers: `{len(client.servers)}`
-> Users: `{len(client.users)}`
-> Uptime: `{str(datetime.timedelta(seconds=int(round(time.time() - uptime))))}s`
-      """,
-      colour="#44ff44"
-    ) # fix the uptime formatting at some point i swear to god
-    await ctx.send(embed=embed)
+    if commands.is_owner:
+      with open("json/data.json", "r") as f:
+        uptime = json.load(f)['uptime']
+      embed = voltage.SendableEmbed(
+        title=f"{client.user.name}'s Stats",
+        description=f"""
+  ## Computer Based Stats
+  > CPU Usage: `{psutil.cpu_percent()}%`
+  > RAM Usage: `{psutil.virtual_memory().percent}%`
+  > Disk Usage: `{psutil.disk_usage('/').percent}%`
+        
+  ## Bot Stats
+  > Servers: `{len(client.servers)}`
+  > Users: `{len(client.users)}`
+  > Uptime: `{str(datetime.timedelta(seconds=int(round(time.time() - uptime))))}s`
+        """,
+        colour="#44ff44"
+      ) # fix the uptime formatting at some point i swear to god
+      await ctx.send(embed=embed)
+    else:
+        await ctx.reply("Not owner, cant use this.")
   
   @owner.command()
-  async def ping(ctx):
-    """Sends Pong!"""
-    start = time.time()
-    embed=voltage.SendableEmbed(
-      title="Pinging..", 
-      description=f"Ping!", 
-      color="#44ff44"
-    )
-    msg = await ctx.reply(content="[]()", embed=embed)
-    for i in range(1,10):
-      embed1 = voltage.SendableEmbed(
-        title="Running PING sequence!",
-        description=f"Ping! `{i}/10`",
-        colour="#44ff44"
-      )
-      await msg.edit(embed=embed1)
-    end = time.time()
-    total = end - start
-    await msg.edit(
+  async def oping(ctx):
+    """Different from normal ping command, this one checks response time and rate limits."""
+    if commands.is_owner:
+      start = time.time()
       embed=voltage.SendableEmbed(
-        title="Pong!",
-        description=f"Pong! in {round(total, 2)}s", # usually this should be 3s - 4s, if its above, you're fucked.
-        colour="#44ff44"
+        title="Pinging..", 
+        description=f"Ping!", 
+        color="#44ff44"
       )
-    )
+      msg = await ctx.reply(content="[]()", embed=embed)
+      for i in range(1,10):
+        embed1 = voltage.SendableEmbed(
+          title="Running PING sequence!",
+          description=f"Ping! `{i}/10`",
+          colour="#44ff44"
+        )
+        await msg.edit(embed=embed1)
+      end = time.time()
+      total = end - start
+      await msg.edit(
+        embed=voltage.SendableEmbed(
+          title="Pong!",
+          description=f"Pong! in {round(total, 2)}s", # usually this should be 3s - 4s, if its above, you're fucked.
+          colour="#44ff44"
+        )
+      )
+    else:
+      await ctx.reply("Not owner, cant use this.")
     
+  @owner.command(
+    name="eval",
+    description="For owner use only! Runs code.",
+    aliases=['ev', 'exec']
+  )
+  async def eval(ctx, *, code):
+    if commands.is_owner:
+      str_obj = io.StringIO() #Retrieves a stream of data
+      try:
+        with contextlib.redirect_stdout(str_obj):
+          exec(code)
+      except Exception as e:
+        return await ctx.send(f"```{e.__class__.__name__}: {e}```")
+      await ctx.send(f'```{str_obj.getvalue()}```')
+    else:
+      await ctx.reply("Not owner, cant use this.")
+    
+  @owner.command(
+    name="kwargstest", 
+    description="working with kwargs sucks, kids.",
+    aliases=['kt', 'okt', 't']
+  )
+  async def kwargstest(ctx, *time, **message):
+    if commands.is_owner:
+      await ctx.send(f"{str(time)}\n{str(message)}")
+    else:
+      await ctx.reply("Not owner, cant use this.")
 
   return owner
--- cogs/utility.py
+++ cogs/utility.py
@@ -1,4 +1,4 @@
-import voltage, pymongo, os, asyncio, json, datetime, time, pendulum
+import voltage, pymongo, os, asyncio, json, datetime, time, pendulum, re
 from voltage.ext import commands
 
 def setup(client) -> commands.Cog:
@@ -24,30 +24,69 @@ def setup(client) -> commands.Cog:
         ) # fix the uptime formatting at some point i swear to god
         await ctx.send(embed=embed)
 
+    def parse_time(time:str):
+        if time:
+            gtime = 0
+            try:
+                if any(x in time for x in ["d", "h", "m", "s"]):
+                    # mlist = list[time] will use this soon :) just gotta get a release out tbh
+                    mtime = re.sub("d|h|m|s", "", time)
+                    if any(x in time for x in ["s", "sec", "seconds"]):
+                        ntime = int(re.sub("d|h|m|s", "", str(time)))
+                        stime = ntime * 1
+                    elif any(x in time for x in ["m", "min", "minutes"]):
+                        ntime = int(re.sub("d|h|m|s", "", str(time)))
+                        mintime = ntime * 60
+                    elif any(x in time for x in ["h", "hrs", "hs", "hours", "hour", "hr"]):
+                        ntime = int(re.sub("d|h|m|s", "", str(time)))
+                        htime = ntime * 3600
+                    elif any(x in time for x in ["d", "day", "da", "days"]):
+                        ntime = int(re.sub("d|h|m|s", "", str(time)))
+                        dtime = ntime * 86400
+                    try:
+                        if stime:
+                            gtime = gtime + int(stime)
+                    except:
+                        pass
+                    try:
+                        if mintime:
+                            gtime = gtime + int(mintime)
+                    except:
+                        pass
+                    try:
+                        if htime:
+                            gtime = gtime + int(htime)
+                    except:
+                        pass
+                    try:
+                        if dtime:
+                            gtime = gtime + int(dtime)
+                    except:
+                        pass
+                    if int(gtime) > 7776000:
+                        return 0
+                    return gtime
+            except Exception as e:
+                print(e)
+
     @utility.command(
         description="🕒 | Set a reminder up to a month! (1d, 1h, 1m, 1s) 'm!reminder 10 'm' do the dishes'",
         name="reminder",
         aliases=["remind", "alert", "timer", "schedule", "setreminder", "setalert", "settimer", "setschedule"]
     )
-    async def reminder(ctx, time:int, timetype, *, reminder):
+    async def reminder(ctx, time, **message):
+        if not message:
+            message = "No message provided."
+        if time:
+            rtime = parse_time(str(time))
         mainembed = voltage.SendableEmbed(
             description=f"""
-Set a reminder: `{reminder}`
-See you in `{time}{timetype}`!
+Set a reminder: `{message}`
+See you in `{time}`!
 """,
             colour="#516BF2",
         )
-        mtime = 0
-        if timetype.lower() in ["s", "sec", "seconds"]:
-            mtime = time
-        elif timetype.lower() in ["m", "min", "minutes"]:
-            mtime = time * 60
-        elif timetype.lower() in ["h", "hrs", "hs", "hours", "hour", "hr"]:
-            mtime = time * 3600
-        elif timetype.lower() in ["d", "day", "da", "days"]:
-            mtime = time * 86400
-        else:
-            mtime = time
+        mtime = time
         if mtime == 0:
             embed = voltage.SendableEmbed(
                 title='Warning',
@@ -55,7 +94,7 @@ See you in `{time}{timetype}`!
                 colour="#B59F3B"
             )
             await ctx.reply(embed=embed, content="[]()")
-        elif mtime > 7776000:
+        elif rtime == 0:
             embed = voltage.SendableEmbed(
                 title='Warning', 
                 description='Your reminder is **too far** in the future!\nMaximum duration is 90 days.',
@@ -64,17 +103,17 @@ See you in `{time}{timetype}`!
             await ctx.reply(embed=embed, content="[]()")
         else:
             msg = await ctx.send(content=ctx.author.mention, embed=mainembed)
-            await asyncio.sleep(mtime)
+            await asyncio.sleep(rtime)
             finished = voltage.SendableEmbed(
                 title=f"Reminded on {pendulum.now().to_day_datetime_string()}!",
                 url=f"https://app.revolt.chat/server/{ctx.server.id}/channels/{ctx.channel.id}/{ctx.message.id}",
-                description=f"`{time}{timetype}` ago you asked me to remind you of `{reminder}`!",
+                description=f"`{time}` ago you asked me to remind you of `{message}`!",
                 colour="#00FF00",
             )
             editwith = voltage.SendableEmbed(
                 title=f"Reminded on {pendulum.now().to_day_datetime_string()}!",
                 url=f"https://app.revolt.chat/server/{ctx.server.id}/channels/{ctx.channel.id}/{ctx.message.id}",
-                description=f"On *{pendulum.now().to_day_datetime_string()}*, {ctx.author.mention} asked me to remind them of:\n`{reminder}`.",
+                description=f"On *{pendulum.now().to_day_datetime_string()}*, {ctx.author.mention} asked me to remind them of:\n`{message}`.",
                 colour="#FF0000",
             )
             await ctx.send(content=ctx.author.mention, embed=finished)
